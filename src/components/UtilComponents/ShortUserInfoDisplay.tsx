0bdde68dea7b1da29f47fd288545087105531d3c
Friedforks <3344544482@qq.com>
2024-07-17 01:27:35 +0800

    Edited ShortUserInfoDisplay.tsx and surrounded tags with <Paper>

---
+++ src/components/UtilComponents/ShortUserInfoDisplay.tsx
@@ -0,0 +1,113 @@
+import { Avatar, Paper, Stack, Typography } from "@mui/material";
+import { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import APIResponse from "../../entity/APIResponse.ts";
+import { FUser } from "../../entity/FUser.ts";
+import axiosInstance from "../../utils/AxiosInstance.ts";
+import {
+    ThumbUpOutlined,
+    VisibilityOutlined,
+    StarBorderOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+type ShortUserInfoDisplayProps = {
+    userId: number | undefined;
+    dataUpdateRequired: boolean;
+    avatarSize?: number;
+};
+export const ShortUserInfoDisplay: React.FC<ShortUserInfoDisplayProps> = ({
+    userId,
+    dataUpdateRequired,
+    avatarSize,
+}) => {
+    const [user, setUser] = useState<FUser>();
+    const [totalView, setTotalView] = useState<number>(0);
+    const [totalLike, setTotalLike] = useState<number>(0);
+    const [totalSave, setTotalSave] = useState<number>(0);
+    useEffect(() => {
+        axiosInstance
+            .get("/fuser/userId", {
+                params: {
+                    userId: userId,
+                    token: localStorage.getItem("token"),
+                },
+            })
+            .then((response: AxiosResponse<APIResponse<FUser>>) => {
+                setUser(response.data.data);
+            });
+        axiosInstance
+            .get("/fuser/total/like", {
+                params: {
+                    userId: userId,
+                },
+            })
+            .then((response: AxiosResponse<APIResponse<number>>) => {
+                setTotalLike(response.data.data);
+            });
+
+        axiosInstance
+            .get("/fuser/total/save", {
+                params: {
+                    userId: userId,
+                },
+            })
+            .then((response: AxiosResponse<APIResponse<number>>) => {
+                setTotalSave(response.data.data);
+            });
+        axiosInstance
+            .get("/fuser/total/view", {
+                params: {
+                    userId: userId,
+                },
+            })
+            .then((response: AxiosResponse<APIResponse<number>>) => {
+                setTotalView(response.data.data);
+            });
+    }, [dataUpdateRequired, userId]);
+    const navigate = useNavigate();
+    return (
+        <Paper elevation={1} sx={{ p: 2 }}>
+            <Stack direction="row" spacing={2}>
+                <div
+                    onClick={() => {
+                        navigate("/user/" + userId);
+                    }}
+                >
+                    {avatarSize ? (
+                        <Avatar
+                            alt="Author avatar"
+                            src={user?.avatarUrl}
+                            style={{ width: avatarSize, height: avatarSize }}
+                        />
+                    ) : (
+                        <Avatar alt="Author avatar" src={user?.avatarUrl} />
+                    )}
+                </div>
+                <Stack direction="column" justifyContent="space-evenly">
+                    <Typography variant="h6">{user?.username}</Typography>
+                    <Stack direction="row" spacing={2} alignItems="flex-end">
+                        <div>
+                            <Stack direction="row" spacing={0.5}>
+                                <ThumbUpOutlined fontSize="small" />
+                                <span>{totalLike}</span>
+                            </Stack>
+                        </div>
+                        <div>
+                            <Stack direction="row" spacing={0.5}>
+                                <VisibilityOutlined fontSize="small" />
+                                <span>{totalView}</span>
+                            </Stack>
+                        </div>
+                        <div>
+                            <Stack direction="row" spacing={0.5}>
+                                <StarBorderOutlined fontSize="small" />
+                                <span>{totalSave}</span>
+                            </Stack>
+                        </div>
+                    </Stack>
+                </Stack>
+            </Stack>
+        </Paper>
+    );
+};
