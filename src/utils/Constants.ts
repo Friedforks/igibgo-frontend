e547df74ea7a884c1384a1065d0d9ce4948df4f2
Friedforks <3344544482@qq.com>
2024-11-22 17:48:02 +0800

    feature: allowed autocomplete for searching tags. Added fixed notes to autocomplete. Added version to CustomHeader1.tsx

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,4 +1,43 @@
-export const Constants ={
-    // requestBaseUrl: "http://localhost:8000",
-    requestBaseUrl: "http://igibgo.cloud:8001",
+export type Tag={
+    label:string;
+    fixed:boolean;
+}
+
+export const Constants = {
+    requestBaseUrl: "http://localhost:8000",
+    // requestBaseUrl: "http://igibgo.cloud:8001",
+    version: "1.3.0",
+    fixedTags: [
+        'IGCSE',
+        'IB',
+        'Computer Science',
+        'Mathematics',
+        'Physics',
+        'Chemistry',
+        'Biology',
+        'ESS',
+        'Sports',
+        'History',
+        'Geography',
+        'Economics',
+        'Business',
+        'Psychology',
+        'Chinese',
+        'English',
+        'Spanish',
+        'Drama',
+        'Music',
+        'Visual Arts',
+        'Design Technology',
+        'Theatre',
+        'CAS',
+        'TOK',
+        'EE',
+        'Paper 1',
+        'Paper 2',
+        'Paper 3',
+        'Coursework',
+        'Internal Assessment',
+        'Individual Oral',
+    ]
 }
